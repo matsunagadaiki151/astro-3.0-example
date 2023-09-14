d542437dc1e4a735b7c5fbb2ecee992cea88421d
matsunagadaiki151 <38464576+matsunagadaiki151@users.noreply.github.com>
2023-09-14 21:57:20 +0900

    view transitionを試す

---
+++ src/blog.ts
@@ -0,0 +1,15 @@
+type TBlog = {
+  id: number;
+  title: string;
+};
+
+export const blogs: TBlog[] = [
+  {
+    id: 1,
+    title: "Astro使ってみた",
+  },
+  {
+    id: 2,
+    title: "AstroのTransitionを使おう",
+  },
+];
